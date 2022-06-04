d6ada4d9bad7b8bab15af2acee6f57ed3083474f
Innam Park <pin954562@gmail.com>
2022-06-04 22:35:48 +0900

    add todo list

---
+++ src/renderer/Tabs/TodoListTab/TodoListCard.tsx
@@ -0,0 +1,66 @@
+import { Dispatch, SetStateAction } from 'react';
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+} from '@mui/material';
+
+interface ITodo {
+  id: number;
+  text: string;
+  state: number;
+}
+
+interface ICardState {
+  code: number;
+  title: string;
+}
+
+interface IProps {
+  todoList: ITodo[];
+  setTodoList: Dispatch<SetStateAction<ITodo[]>>;
+  cardState: ICardState;
+}
+
+export default function TodoListCard({
+  todoList,
+  setTodoList,
+  cardState,
+}: IProps) {
+  const handleChange = (id: number) => {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, state: todo.state + 1 } : todo
+      )
+    );
+  };
+
+  return (
+    <Card>
+      <CardHeader title={cardState.title} />
+      <CardActions>
+        <FormGroup>
+          {todoList.map(
+            (todo) =>
+              todo.state === cardState.code && (
+                <FormControlLabel
+                  key={todo.id}
+                  control={
+                    <Checkbox
+                      onChange={() => {
+                        handleChange(todo.id);
+                      }}
+                    />
+                  }
+                  label={todo.text}
+                />
+              )
+          )}
+        </FormGroup>
+      </CardActions>
+    </Card>
+  );
+}
